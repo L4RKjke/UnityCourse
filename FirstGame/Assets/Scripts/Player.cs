519bd6be744b263fd9b4317f87548fb7a2fe6ed9
L4RKjke <nik.trenikhin@gmail.com>
2022-08-12 18:49:55 +0500

    Game
    v4.1

--- FirstGame/Assets/Scripts/Player.cs
+++ FirstGame/Assets/Scripts/Player.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private int _coinGoal = 3;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Coin coin))
+        {
+            CollectCoin(coin);
+        }
+    }
+
     public void DestoyEntity()
     {
         Destroy(gameObject);
     }
+
+    private void CollectCoin(Coin coin)
+    {
+        coin.DestoyEntity();
+        _coinGoal -= 1;
+    } 
+
 }
